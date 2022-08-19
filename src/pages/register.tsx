21a3ac6fe4dc201d4b4403fc2916947f330d7b3a
ali <aliwolfi18@gmail.com>
2022-08-20 01:05:19 +0430

    little styling for register page

--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -20,27 +20,32 @@ export default function Register() {
 
     return (
         <>
-            <form className='grid justify-items-center '
-                  onSubmit={handleSubmit(onSubmit)}>
-                {error && error.message}
-                <div className='p-4 text-lg'>Register</div>
-                <input
-                    type="email"
-                    placeholder='mark.zackerberg@facebook.com'
-                    {...register('email')}
-                />
-                <br/>
-                <input
-                    type="text"
-                    placeholder='mark'
-                    {...register('name')}
-                />
-                <button className='bg-blue-50 px-4 py-2 rounded-md hover:bg-blue-100' type={'submit'}>Register</button>
-            </form>
+            <div className='w-full flex justify-start items-center flex-col mt-16'>
+                <div className='border-2 dark:border-zinc-200 border-zinc-900 p-4 rounded-md'>
+                    <form className='flex flex-col  items-start'
+                          onSubmit={handleSubmit(onSubmit)}>
+                        {error && error.message}
+                        <div className='text-lg mb-4'>Register</div>
+                        <input
+                            type="email"
+                            placeholder='mark.zackerberg@facebook.com'
+                            {...register('email')}
+                        />
+                        <br/>
+                        <input
+                            className='mb-6'
+                            type="text"
+                            placeholder='mark'
+                            {...register('name')}
+                        />
+                        <button className='flex justify-center items-center w-full py-2 bg-green-400 dark:text-zinc-900 text-zinc-200 rounded-md mb-4' type={'submit'}>Register</button>
+                    </form>
 
-            <Link href='/login'>
-                <div className='grid justify-items-center text-blue-400 cursor-pointer'>Login</div>
-            </Link>
+                    <Link href='/login'>
+                        <div className='text-blue-400 cursor-pointer'>Login</div>
+                    </Link>
+                </div>
+            </div>
         </>
     )
 }
